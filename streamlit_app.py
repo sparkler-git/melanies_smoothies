0500a71d6d1d3890a56cc9458afcd2d69940c336
sparkler-git <lightningsparks001@gmail.com>
2024-12-17 20:29:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ if ingredient_list:
          
          st.subheader(fruit_chosen + 'Nutrition Information')
          
-         smoothiefroot_response=requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
+         smoothiefroot_response=requests.get("https://my.smoothiefroot.com/api/fruit/"+ search_on)
          sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
      #st.write(ingredient_string)
 
