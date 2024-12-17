4060da2df3022df09d292fb8d5bfddce902da6f2
sparkler-git <lightningsparks001@gmail.com>
2024-12-17 19:45:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+
 
 
 # Write directly to the app
@@ -43,9 +45,6 @@ if ingredient_list:
      if ingredient_string:
          session.sql(my_insert_stmt).collect()
          st.success('Your Smoothie is ordered!', icon="✅")
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
          
 
